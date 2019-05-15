0e204b8ffe471ed09cc22ba465f119bbbec1f0a2
Dave <redirwin@gmail.com>
2019-05-14 20:44:29 -0700

    WIP - Completed 10 event listeners.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,78 @@
 // Your code goes here
+const logoText = document.querySelector('h1');
+const navLinks = document.querySelectorAll('nav a');
+const contentHeaders = document.querySelectorAll('h2');
+const destination = document.querySelectorAll('.destination');
+const footer = document.querySelector('footer');
+
+logoText.addEventListener('mouseover',
+
+    event => {
+        event.target.classList.add('busColors');
+    })
+
+logoText.addEventListener('mouseout', event => {
+    event.target.classList.remove('busColors');
+})
+
+document.addEventListener('keydown', event => {
+    navLinks.forEach(function (navItem) {
+        navItem.classList.add('busColors');
+        navItem.style.fontSize = '2rem';
+        navItem.style.color = 'yellow';
+    })
+})
+
+document.addEventListener('keyup', event => {
+    navLinks.forEach(function (navItem) {
+        navItem.classList.remove('busColors');
+        navItem.style.fontSize = '1.75rem';
+        navItem.style.color = 'black';
+    })
+})
+
+document.addEventListener('wheel', event => {
+    contentHeaders.forEach(function (header) {
+        header.classList.add('busColors');
+        header.style.fontSize = '2.5rem';
+        header.style.color = 'yellow';
+    })
+})
+
+window.addEventListener('mousedown', event => {
+    // console.log('mouse is down');
+    event.target.style.backgroundColor = 'black';
+    event.target.style.color = 'yellow';
+})
+
+window.addEventListener('mouseup', event => {
+    // console.log('mouse is down');
+    event.target.style.backgroundColor = 'white';
+    event.target.style.color = 'black';
+})
+
+// Thank you, Skyelar!
+const imageDrag = document.querySelectorAll('img');
+
+imageDrag.forEach(img => {
+    img.addEventListener('drag', event => {
+        event.currentTarget.style.filter = 'grayscale(100%)';
+    })
+})
+
+const nav = document.querySelector('.nav')
+
+const printBtn = document.createElement('button');
+
+printBtn.textContent = 'Print This Page';
+
+footer.appendChild(printBtn);
+
+printBtn.onclick = function () {
+    window.print();
+}
+
+window.addEventListener('beforeprint', (event) => {
+    alert('Click OK to print.');
+})
+
